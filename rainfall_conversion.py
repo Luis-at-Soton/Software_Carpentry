8530f6b14aaed81a2617ad587865f900b289a485
Lydia Doherty <lydiadoherty1@gmail.com>
2019-12-17 14:50:28 +0000

    Add rainfall module - Wetty for wetty

---
+++ rainfall_conversion.py
@@ -0,0 +1,11 @@
+""" A library to perform rainfall unit conversion"""
+
+def inches_to_mm(inches):
+    """Convert inches to mm
+
+    Arguments: 
+    inches -- the rainfall inches 
+    """It all matters"""
+    """
+    mm = inches * 25.4
+    return mm
